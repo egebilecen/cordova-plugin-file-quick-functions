42e3462d42e54338a47f9c4daa744e8c5fb999ab
Ege Bilecen <egebilecen@icloud.com>
2017-09-03 13:07:27 +0300

    Code improvements

--- file_operations.js
+++ file_operations.js
@@ -5,10 +5,7 @@
 */
 var FILE_RETURN; //true if operation was successful, false on fail
 var MEMORY = { //file datas will be in "fileData" attribute.
-	fileData : {}
-};
-var SETTINGS = {
-	debug : 1
+    fileData : {}
 };
 
 /*
@@ -26,31 +23,111 @@ var SETTINGS = {
 */
 
 //saves file's data that send as name parameter on function if used alone.
-function getFile( name, save,successFunc, errorFunc ){
-    if( typeof name == 'undefined' )
+function getFile( name, save, successFunc, errorFunc ){
+    if( typeof name === 'undefined' )
         return false;
-    if( typeof save == 'undefined' )
-        save = 'temporary';
-    if( typeof successFunc != 'function' ||  typeof successFunc == 'undefined' )
+    if( typeof save === 'undefined' || typeof save !== "string" )
+        save = null;
+    if( typeof successFunc !== 'function' ||  typeof successFunc === 'undefined' )
         successFunc = function(){ if(SETTINGS.debug){console.log('success - getFile');} }
-    if( typeof errorFunc != 'function'   ||  typeof errorFunc == 'undefined' )
+    if( typeof errorFunc !== 'function'   ||  typeof errorFunc === 'undefined' )
         errorFunc = function(){ if(SETTINGS.debug){console.log('error - getFile');} }
-    
+
     window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, function (fs) {
         fs.root.getFile(name, { create: false, exclusive: false }, function (fileEntry) {
-            if(SETTINGS.debug){console.info('file - file found, reading');}
-            readFile(fileEntry, save, successFunc, errorFunc);
+            if( save !== null )
+            {
+                if(SETTINGS.debug){console.info('file - file found, reading');}
+                readFile(fileEntry, save, successFunc, errorFunc);
+            }
+            else
+                successFunc();
         }, function(){if(SETTINGS.debug){console.info('file - error2 file not exist');} FILE_RETURN=false;errorFunc();});
     }, function(){if(SETTINGS.debug){console.info('file - error1 - cannot load filesystem');} FILE_RETURN=false;errorFunc();});
 }
 
+function createFile( name, successFunc, errorFunc ){
+    if( typeof name === 'undefined' )
+        return false;
+    if( typeof successFunc !== 'function' ||  typeof successFunc === 'undefined' )
+        successFunc = function(){ if(SETTINGS.debug){console.log('success - createFile');} }
+    if( typeof errorFunc !== 'function'   ||  typeof errorFunc === 'undefined' )
+        errorFunc = function(){ if(SETTINGS.debug){console.log('error - createFile');} }
+
+    window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, function (fs) {
+        fs.root.getFile(name, { create: true, exclusive: false }, function (fileEntry) {
+            if(SETTINGS.debug){console.info('file - file created');}
+            FILE_RETURN=true;
+            successFunc();
+        }, function(){if(SETTINGS.debug){console.info('file - error2 file not exist');} FILE_RETURN=false; errorFunc();});
+    }, function(){if(SETTINGS.debug){console.info('file - error1 - cannot load filesystem');} FILE_RETURN=false;errorFunc();});
+}
+
+//@param text: Data that will be writed to file.\\
+function writeFile( name, text, save, successFunc, errorFunc ) {
+    if( typeof name === 'undefined' )
+        return false;
+    if( typeof text === 'undefined' )
+        text = '';
+    if( typeof save === 'undefined' || typeof save !== "string" )
+        save = null;
+    if( typeof successFunc !== 'function' ||  typeof successFunc === 'undefined' )
+        successFunc = function(){ if(SETTINGS.debug){console.log('success - writeFile');} }
+    if( typeof errorFunc !== 'function'   ||  typeof errorFunc === 'undefined' )
+        errorFunc = function(){ if(SETTINGS.debug){console.log('error - writeFile');} }
+
+    window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, function (fs) {
+        fs.root.getFile(name, { create: false, exclusive: false }, function (fileEntry) {
+
+            fileEntry.createWriter(function (fileWriter) {
+
+                fileWriter.onwriteend = function() {
+                    if(SETTINGS.debug){console.info("file - successfully writed");}
+                    if( save !== null )
+                        readFile(fileEntry, save, successFunc, errorFunc);
+                };
+
+                fileWriter.onerror = function (e) {
+                    if(SETTINGS.debug){console.info("file - error - write failed to "+e.toString());}
+                    FILE_RETURN=false;
+                    errorFunc();
+                };
+
+                // If data object is not passed in,\\
+                // create a new Blob instead.\\
+                dataObj = new Blob([text], { type: 'text/plain' });
+
+                fileWriter.write(dataObj);
+            });
+
+        }, function(){if(SETTINGS.debug){console.info('file - error2 file not exist');} FILE_RETURN=false;errorFunc();});
+    }, function(){if(SETTINGS.debug){console.info('file - error1 - cannot load filesystem');} FILE_RETURN=false;errorFunc();});
+
+}
+
+function clearFile( name, successFunc, errorFunc ){
+    if( typeof name === 'undefined' )
+        return false;
+    if( typeof successFunc !== 'function' ||  typeof successFunc === 'undefined' )
+        successFunc = function(){ if(SETTINGS.debug){console.log('success - createFile');} }
+    if( typeof errorFunc !== 'function'   ||  typeof errorFunc === 'undefined' )
+        errorFunc = function(){ if(SETTINGS.debug){console.log('error - createFile');} }
+
+    getFile(name,'temporary',function(){
+        var a = '';
+        for( var i=1; i <= MEMORY.fileData.temporary.length; i++ )
+        { a += ' '; }
+        writeFile(name,a,'temporary',function(){ successFunc(); },function(){ errorFunc(); });
+    });
+}
+
 //!!! Don't use that function. It's a helper function. !!!\\
 function readFile( fileEntry, save, successFunc, errorFunc ) {
-    if( typeof save == 'undefined' )
+    if( typeof save === 'undefined' )
         save = 'temporary';
-    if( typeof successFunc != 'function' ||  typeof successFunc == 'undefined' )
+    if( typeof successFunc !== 'function' ||  typeof successFunc === 'undefined' )
         successFunc = function(){ if(SETTINGS.debug){console.log('success - readFile');} }
-    if( typeof errorFunc != 'function'   ||  typeof errorFunc == 'undefined' )
+    if( typeof errorFunc !== 'function'   ||  typeof errorFunc === 'undefined' )
         errorFunc = function(){ if(SETTINGS.debug){console.log('error - readFile');} }
 
     fileEntry.file(function (file) {
@@ -66,79 +143,4 @@ function readFile( fileEntry, save, successFunc, errorFunc ) {
             successFunc();
         };
     }, function(){ if(SETTINGS.debug){console.info('file - error3 - cannot read file');} FILE_RETURN=false; errorFunc(); });
-}
-
-//@param text: Data that will be writed to file.\\
-function writeFile( name, text, save,successFunc, errorFunc ) {
-    if( typeof name == 'undefined' )
-        return false;
-    if( typeof save == 'undefined' )
-        save = 'temporary';
-    if( typeof text == 'undefined' )
-        text = '';
-    if( typeof successFunc != 'function' ||  typeof successFunc == 'undefined' )
-        successFunc = function(){ if(SETTINGS.debug){console.log('success - writeFile');} }
-    if( typeof errorFunc != 'function'   ||  typeof errorFunc == 'undefined' )
-        errorFunc = function(){ if(SETTINGS.debug){console.log('error - writeFile');} }
-
-    window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, function (fs) {
-        fs.root.getFile(name, { create: false, exclusive: false }, function (fileEntry) {
-            
-            fileEntry.createWriter(function (fileWriter) {
-
-            fileWriter.onwriteend = function() {
-                if(SETTINGS.debug){console.info("file - successfully writed");}
-                readFile(fileEntry, save, successFunc, errorFunc);
-            };
-
-            fileWriter.onerror = function (e) {
-                if(SETTINGS.debug){console.info("file - error - write failed to "+e.toString());}
-                FILE_RETURN=false;
-                errorFunc();
-            };
-
-            // If data object is not passed in,\\
-            // create a new Blob instead.\\
-            dataObj = new Blob([text], { type: 'text/plain' });
-
-            fileWriter.write(dataObj);
-        });
-
-        }, function(){if(SETTINGS.debug){console.info('file - error2 file not exist');} FILE_RETURN=false;errorFunc();});
-    }, function(){if(SETTINGS.debug){console.info('file - error1 - cannot load filesystem');} FILE_RETURN=false;errorFunc();});
-    
-}
-
-function createFile( name, successFunc, errorFunc ){
-    if( typeof name == 'undefined' )
-        return false;
-    if( typeof successFunc != 'function' ||  typeof successFunc == 'undefined' )
-        successFunc = function(){ if(SETTINGS.debug){console.log('success - createFile');} }
-    if( typeof errorFunc != 'function'   ||  typeof errorFunc == 'undefined' )
-        errorFunc = function(){ if(SETTINGS.debug){console.log('error - createFile');} }
-
-    window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, function (fs) {
-        fs.root.getFile(name, { create: true, exclusive: false }, function (fileEntry) {
-            if(SETTINGS.debug){console.info('file - file created');}
-            FILE_RETURN=true;
-            successFunc();
-        }, function(){if(SETTINGS.debug){console.info('file - error2 file not exist');} FILE_RETURN=false; errorFunc();});
-    }, function(){if(SETTINGS.debug){console.info('file - error1 - cannot load filesystem');} FILE_RETURN=false;errorFunc();});
-}
-
-//If you want to overwrite to exist file, first use this function to clear it. And then use writeFile() function.
-function clearFile( name, successFunc, errorFunc ){
-    if( typeof name == 'undefined' )
-        return false;
-    if( typeof successFunc != 'function' ||  typeof successFunc == 'undefined' )
-        successFunc = function(){ if(SETTINGS.debug){console.log('success - createFile');} }
-    if( typeof errorFunc != 'function'   ||  typeof errorFunc == 'undefined' )
-        errorFunc = function(){ if(SETTINGS.debug){console.log('error - createFile');} }
-
-    getFile(name,'temporary',function(){
-        var a = '';
-        for( var i=1; i <= MEMORY.fileData.temporary.length; i++ )
-        { a += ' '; }
-        writeFile(name,a,'temporary',function(){ successFunc(); },function(){ errorFunc(); });
-    });
 }
